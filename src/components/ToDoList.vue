e697bf265e323dba3c1923700e90d5602744be69
Emaleff <emalev91@gmail.com>
2022-04-03 00:45:34 +0700

    add h1

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -1,8 +1,9 @@
 <template>
-  <div class="to-do__list">
+  <div class="to-do__list" v-if="getTasks.length > 0">
     <ToDoItem v-for="task in getSelectedTodo" :key="task.id" :task="task" />
     <ToDoPanel />
   </div>
+  <h1 v-else class="to-do__title">Список задач пуст, добавьте задачу</h1>
 </template>
 
 <script>
@@ -12,7 +13,7 @@ import { mapGetters } from "vuex";
 export default {
   name: "ToDoList",
   computed: {
-    ...mapGetters(["getSelectedTodo"]),
+    ...mapGetters(["getSelectedTodo", "getTasks"]),
   },
   components: {
     ToDoPanel,
@@ -27,5 +28,9 @@ export default {
   border: 1px solid gray;
   margin: 0 auto 50px;
   border-radius: 5px;
+}
+.to-do__title {
+  margin: 20px;
+  text-align: center;
 }
 </style>
